b1374a2f6dfee1de68ffd1ca89b1eab61d9d3ffa
dkunz60 <kunzd60@gmail.com>
2025-03-02 10:44:42 -0500

    Update cu_conditions_without_mqtt.py

--- cu_conditions_without_mqtt.py
+++ cu_conditions_without_mqtt.py
@@ -1,5 +1,4 @@
 import time
-import sys
 
 connection_status = False
 user_input = False
@@ -27,28 +26,33 @@ ovr_str = ""
 # Wait for data input
 while user_input == False:
     data = input("Enter data string: ")
-    user_input = True
-    
-# Parse PM2.5 data
-parse_2 = data.split(start_2)
-if len(parse_2) > 1:
-    PM2_str = parse_2[1].split(end_2)[0]
-    print(PM2_str)
-    IAQ_PM2 = int(PM2_str)
+        
+    # Parse PM2.5 data
+    parse_2 = data.split(start_2)
+    if len(parse_2) > 1:
+        PM2_str = parse_2[1].split(end_2)[0]
+        print(PM2_str)
+        IAQ_PM2 = int(PM2_str)
 
-# Parse PM10 data
-parse_10 = data.split(start_10)
-if len(parse_10) > 1:
-    PM10_str = parse_10[1].split(end_10)[0]
-    print(PM10_str)
-    IAQ_PM10 = int(PM10_str)
-    
-# Parse Overall PM data
-parse_ovr = data.split(start_ovr)
-if len(parse_ovr) > 1:
-    ovr_str = parse_ovr[1].split(end_ovr)[0]
-    print(ovr_str)
-    IAQ_ovr = int(ovr_str)
+    # Parse PM10 data
+    parse_10 = data.split(start_10)
+    if len(parse_10) > 1:
+        PM10_str = parse_10[1].split(end_10)[0]
+        print(PM10_str)
+        IAQ_PM10 = int(PM10_str)
+        
+    # Parse Overall PM data
+    parse_ovr = data.split(start_ovr)
+    if len(parse_ovr) > 1:
+        ovr_str = parse_ovr[1].split(end_ovr)[0]
+        print(ovr_str)
+        IAQ_ovr = int(ovr_str)
+        
+    if IAQ_ovr == 0 and IAQ_PM2 == 0 and IAQ_PM10 == 0:
+        print("Invalid data entry")
+    else:
+        user_input = True
+        
     
 while connection_status == True:
     time.sleep(0.5)
