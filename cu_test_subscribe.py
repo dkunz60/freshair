25274ae8858b8bd24117110a68444a578417d636
dkunz60 <kunzd60@gmail.com>
2025-03-02 16:14:39 -0500

    Add files via upload

---
+++ cu_test_subscribe.py
@@ -0,0 +1,161 @@
+# Initialize libraries
+import time
+import paho.mqtt.client as paho
+import sys
+import RPi.GPIO as GPIO
+from paho import mqtt
+
+# Initialize variables
+connection_status = False
+new_data = False
+user_input = False
+Sensor_1 = False
+Sensor_2 = False
+Sensor_3 = False
+IAQ_PM2 = 0
+IAQ_PM10 = 0
+IAQ_ovr = 0
+
+# Raw data from sensors comes in the following format:
+# {"IAQI_PM2.5":12,"IAQI_PM10":2,"Overall_IAQI":12}
+# NOTE: Numeric values between ":" and "," characters will change
+# depending on real time data
+
+# Data string parsing variables
+start_2 = "{\"IAQI_PM2.5\":"
+end_2 = ",\"IAQI_PM10"
+start_10 = "IAQI_PM10\":"
+end_10 = ",\"Overall"
+start_ovr = "Overall_IAQI\":"
+end_ovr = "}"
+PM2_str = ""
+PM10_str = ""
+ovr_str = ""
+
+# Upon connection, print connection status and set global variable
+def on_connect(client, userdata, flags, rc, properties=None):
+    # THE FOLLOWING EXCERPT IS ADAPTED FROM AN AI GENERATED EXAMPLE:
+    global connection_status
+    if rc == 0:
+        print("Successfully connected to the broker")
+        connection_status = True
+    else:
+        print("Failed to connect, code %d" %rc)
+        connection_status = False
+    # END OF AI GENERATED CODE
+    
+# Upon published message, print confirmation
+def on_publish(client, userdata, mid, properties=None):
+    print("mid: " + str(mid))
+    
+# Upon subscription, print confirmation
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+# print message, useful for checking if it was successful
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    
+    # using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
+# userdata is user defined data of any type, updated by user_data_set()
+# client_id is the given name of the client
+client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+client.on_connect = on_connect
+
+# enable TLS for secure connection
+client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+# set username and password
+client.username_pw_set("freshair", "Freshair1")
+# connect to HiveMQ Cloud on port 8883 (default for MQTT)
+client.connect("307235db588f42bda5a2c31d242ef05c.s1.eu.hivemq.cloud", 8883)
+
+# setting callbacks, use separate functions like above for better visibility
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.on_publish = on_publish
+
+while connection_status == True:
+    time.sleep(0.5)
+    # If acknowledged, turn off sirens
+    if toggle == True:
+            GPIO.output(5, False)
+            GPIO.output(6, False)
+            
+    # Wait for data input
+    while user_input == False:
+        client.subscribe("Sensor1/data", qos=0)
+        client.subscribe("Sensor2/data", qos=0)
+        client.subscribe("Sensor3/data", qos=0)
+        # Parse PM2.5 data
+        parse_2 = data.split(start_2)
+        if len(parse_2) > 1:
+            PM2_str = parse_2[1].split(end_2)[0]
+            print(PM2_str)
+            # Try to convert string to int type
+            try:
+                IAQ_PM2 = int(PM2_str)
+            # If error, something is wrong with entry
+            except ValueError:
+                print("Invalid data entry")
+                pass          
+
+        # Parse PM10 data
+        parse_10 = data.split(start_10)
+        if len(parse_10) > 1:
+            PM10_str = parse_10[1].split(end_10)[0]
+            print(PM10_str)
+            # Try to convert string to int type
+            try:
+                IAQ_PM10 = int(PM10_str)
+            # If error, something is wrong with entry
+            except ValueError:
+                print("Invalid data entry")
+                pass
+            
+        # Parse Overall PM data
+        parse_ovr = data.split(start_ovr)
+        if len(parse_ovr) > 1:
+            ovr_str = parse_ovr[1].split(end_ovr)[0]
+            print(ovr_str)
+            # Try to convert string to int type
+            try:
+                IAQ_ovr = int(ovr_str)
+            # If error, something is wrong with entry
+            except ValueError:
+                print("Invalid data entry")
+                pass
+        
+        # If all data is still zero, something is wrong, ask for data again
+        if IAQ_ovr == 0 and IAQ_PM2 == 0 and IAQ_PM10 == 0:
+            print("Invalid data entry")
+        # Else, data is good, strings were parsed, continue
+        else:
+            new_data = True
+            user_input = True
+                   
+        
+    while new_data == True:
+        time.sleep(0.5)
+        # If over/equal to conditions below, "Dangerous" PM condition met (via OSHA 1910.1000)
+        if IAQ_PM2 >= 55 or IAQ_PM10 >= 255 or IAQ_ovr >=255:
+            print("Dangerous")
+            # GPIO 5
+            new_data = False
+            user_input = False
+        # If under "Dangerous" threshold and over/equal to conditions below, "Unhealthy" PM condition met (via OSHA 1910.1000)
+        elif IAQ_PM2 >= 35 or IAQ_PM10 >= 155 or IAQ_ovr >=155:
+            print("Unhealthy")
+            # GPIO 6
+            new_data = False
+            user_input = False
+        # If below both conditions, "Healthy" PM condition met (via OSHA 1910.1000)
+        else:
+            print("Healthy")
+            new_data = False
+            user_input = False
+            toggle = True
+            
+        # If acknowledged, turn off sirens
+        if toggle == True:
+            GPIO.output(5, False)
+            GPIO.output(6, False)
