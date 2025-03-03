96c83235725f461b8b0f2d19c2b83f3dfd7c4f8e
dkunz60 <kunzd60@gmail.com>
2025-03-03 17:59:05 -0500

    Update cu_test_main.py

--- cu_test_main.py
+++ cu_test_main.py
@@ -1,14 +1,34 @@
+# Initialize libraries
 import time
 import paho.mqtt.client as paho
 import sys
 import RPi.GPIO as GPIO
 from paho import mqtt
+import json
 
+# Initialize variables
+toggle = False
 connection_status = False
+new_data = False
+user_input = False
+Sensor_1 = False
+Sensor_2 = False
+Sensor_3 = False
+received = False
+vent = False
+data = {}
 IAQ_PM2 = 0
 IAQ_PM10 = 0
-IAQ_Overall = 0
-Sensor_ID = 0
+IAQ_ovr = 0
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(16,GPIO.OUT, initial = False)                  
+GPIO.setup(18,GPIO.OUT, initial = False)
+
+# Raw data from sensors comes in the following format:
+# {"iaqi_pm2.5":12,"iaqi_pm10":2,"overall_iaqi":12}
+# NOTE: Numeric values between ":" and "," characters will change
+# depending on real time data
 
 # Upon connection, print connection status and set global variable
 def on_connect(client, userdata, flags, rc, properties=None):
@@ -30,66 +50,158 @@ def on_publish(client, userdata, mid, properties=None):
 def on_subscribe(client, userdata, mid, granted_qos, properties=None):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
-# Upon message, print confirmation
+# Print message if data is received from sensor
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-    Sensor_ID = msg.topic
-    print("ID" %s)
-    if Sensor_ID = 1:
-        client.publish("cu/id", payload="Sensor 1", qos=1)
-        GPIO.output(5, True)
-        GPIO.output(6, False)
-        GPIO.output(6, False)
-    elif Sensor_ID = 2:
-        client.publish("cu/id", payload="Sensor 2", qos=1)
-        GPIO.output(5, False)
-        GPIO.output(6, True)
-        GPIO.output(6, False)
-    else:
-        client.publish("cu/id", payload="Sensor 3", qos=1)
-        GPIO.output(5, False)
-        GPIO.output(6, False)
-        GPIO.output(6, True)
-        
+    global data
+    global received
+    global toggle
+    global vent
+    global Sensor_1
+    global Sensor_2
+    global Sensor_3
     
-# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
-# userdata is user defined data of any type, updated by user_data_set()
-# client_id is the given name of the client
+    # If message from Toggle, button was pressed, set to true
+    if "Toggle" in msg.topic:
+        toggle = True
+        
+    # If message from Ventilation, set equal to message value
+    if "Vent" in msg.topic:
+        vent_payload = msg.payload.decode("utf-8").strip().lower()
+    
+        if vent_payload in ['true', '1', b'true', b'1']:
+            vent = True
+        elif vent_payload in ['false', '0', b'false', b'0']:
+            vent = False
+        else:
+            vent = False
+        
+        received = True
+        
+    # If message is from sensor, parse data 
+    if "Sensor" in msg.topic:
+        payload = msg.payload.decode("utf-8")
+        data = json.loads(payload)
+        print("Received message: ", data)
+        received = True
+        
+    # Determine sensor ID
+    if "Sensor1" in msg.topic:
+        Sensor_1 = True
+    if "Sensor2" in msg.topic:
+        Sensor_2 = True
+    if "Sensor3" in msg.topic:
+        Sensor_3 = True
+     
+# MQTT Initialization
 client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
-
-# Set up for connection function
 client.on_connect = on_connect
-
-# enable TLS for secure connection
 client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-# set username and password
 client.username_pw_set("freshair", "Freshair1")
-# connect to HiveMQ Cloud on port 8883 (default for MQTT)
 client.connect("307235db588f42bda5a2c31d242ef05c.s1.eu.hivemq.cloud", 8883)
-# Set up for publish function
+client.on_subscribe = on_subscribe
+client.on_message = on_message
 client.on_publish = on_publish
-
-# Begin loop for broker
+client.subscribe("Sensor1/#", qos=0)
+client.subscribe("Sensor2/#", qos=0)
+client.subscribe("Sensor3/#", qos=0)
+client.subscribe("Toggle/#", qos=0)
+client.subscribe("Vent/#", qos=0)
 client.loop_start()
 
-# If connected, go through the following conditions:
-while True:
-    time.sleep(0.5)
-    client.subscribe("Sensor1/#", qos=0)
-    # If over/equal to 200 IAQ, "Dangerous" PM condition met
-    if IAQ_PM2 >= 200 or IAQ_PM10 >= 200 or IAQ_Overall >=200:
-        client.publish("cu/control", payload="Dangerous", qos=1)
-        GPIO.output(5, True)
-        GPIO.output(6, False)
-    # If under 200 and over/equal to 100, "Unhealthy" PM condition met
-    elif IAQ_PM2 >= 100 or IAQ_PM10 >= 100 or IAQ_Overall >=100:
-        client.publish("cu/control", payload="Unhealthy", qos=1)
-        GPIO.output(5, False)
-        GPIO.output(6, True)
-    # If below both conditions, "Healthy" PM condition met
-    else:
-        client.publish("cu/control", payload="Healthy", qos=1)
-        GPIO.output(5, False)
-        GPIO.output(6, False)
+# Wait on broker connection
+while connection_status == False:
+    print("Waiting for connection...")
+    time.sleep(1)
 
+# Broker successfully connected
+while connection_status == True:
+    time.sleep(0.5)
+    
+    # If acknowledged, turn off sirens
+    while toggle == True:
+        GPIO.output(18, False)
+        time.sleep(0.5)
+        GPIO.output(16, False)
+        time.sleep(0.5)
+        print("Siren Status: OFF")
+        toggle = False
+            
+    # If data received
+    while received == True:
+        if Sensor_1 == True or Sensor_2 == True or Sensor_3 == True:
+            # Parse sensor data
+            IAQ_PM2 = data.get("iaqi_pm2.5", 0)
+            IAQ_PM10 = data.get("iaqi_10", 0)
+            IAQ_ovr = data.get("overall_iaqi", 0)
+            new_data = True
+            received = False
+        
+        # Handle ventilation status
+        if vent == True:
+            client.publish("cu/vent", payload=True, qos=0)
+            print("Ventilation Status: ON")
+            received = False
+        if vent == False:
+            client.publish("cu/vent", payload=False, qos=0)
+            print("Ventilation Status: OFF")
+            received = False
+    
+    # If sensor data is parsed:   
+    if new_data:
+        time.sleep(0.5)
+        
+        # Publish individual values to broker
+        client.publish("cu/IAQ_PM2.5", payload=IAQ_PM2, qos=0)
+        client.publish("cu/IAQ_PM10", payload=IAQ_PM10, qos=0)
+        client.publish("cu/IAQ_ovr", payload=IAQ_ovr, qos=0)
+        
+        # If over/equal to conditions below, "Dangerous" PM condition met (via OSHA 1910.1000)
+        if IAQ_PM2 >= 55 or IAQ_PM10 >= 255 or IAQ_ovr >=255:
+            client.publish("cu/pm_status", payload="Dangerous", qos=0)
+            time.sleep(0.5)
+            GPIO.output(18, True)
+            time.sleep(0.5)
+            client.publish("cu/vent", payload=True, qos=0)
+            print("Ventilation Status: ON")
+            print("Siren Status: RED")
+            time.sleep(5)
+            new_data = False
+            
+        # If under "Dangerous" threshold and over/equal to conditions below, "Unhealthy" PM condition met (via OSHA 1910.1000)
+        elif IAQ_PM2 >= 35 or IAQ_PM10 >= 155 or IAQ_ovr >=155:
+            client.publish("cu/pm_status", payload="Unhealthy", qos=0)
+            print("Siren Status: YELLOW")
+            time.sleep(0.5)
+            GPIO.output(16, True)
+            time.sleep(5)
+            new_data = False
+            
+        # If below both conditions, "Healthy" PM condition met (via OSHA 1910.1000)
+        else:
+            client.publish("cu/pm_status", payload="Healthy", qos=0)
+            client.publish("cu/vent", payload=False, qos=0)
+            GPIO.output(18, False)
+            time.sleep(0.5)
+            GPIO.output(16, False)
+            time.sleep(0.5)
+            time.sleep(5)
+            print("Ventilation Status: OFF")
+            print("Siren Status: OFF")
+            toggle = True
+            new_data = False
+            
+            
+        # Publish sensor ID of the data to the broker    
+        if Sensor_1 == True:
+            client.publish("cu/sensor_id", payload="Sensor 1", qos=0)
+            print("Data received from Sensor 1")
+        if Sensor_2 == True:
+            client.publish("cu/sensor_id", payload="Sensor 2", qos=0)
+            print("Data received from Sensor 2")
+        if Sensor_3 == True:
+            client.publish("cu/sensor_id", payload="Sensor 3", qos=0)
+            print("Data received from Sensor 3")           
+            
+GPIO.cleanup()
+client.loop_stop()
 sys.exit()
